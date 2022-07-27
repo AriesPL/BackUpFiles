d53bcd1e42675fd8e9e4a959f34625a1af6918ea
AriesPL <blackhorns1989@gmail.com>
2022-07-27 13:56:29 +0300

    feat: создание копий в папке BackUp

--- BackUpFiles/Program.cs
+++ BackUpFiles/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace BackUpFiles
 {
 	internal class Program
 	{
+		private static string pathToOriginalFolder = "C:/Users/Алексей/source/repos/BackUpFiles/BackUpFiles/original";
+		private static string pathToBackUpFolder = "BackUp";
+
 		static void Main(string[] args)
 		{
-		}
-	}
+            string fileName = "1.txt";
+
+            string sourceFile = Path.Combine(pathToOriginalFolder, fileName);
+            string destFile = Path.Combine(pathToBackUpFolder, fileName);
+
+            Directory.CreateDirectory(pathToBackUpFolder);
+
+            File.Copy(sourceFile, destFile, true);
+
+            if (Directory.Exists(pathToOriginalFolder))
+            {
+                string[] files = Directory.GetFiles(pathToOriginalFolder);
+
+                foreach (string file in files)
+                {
+                    fileName = Path.GetFileName(file);
+                    destFile = Path.Combine(pathToBackUpFolder, fileName);
+                    File.Copy(file, destFile, true);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Source path does not exist!");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
 }
+
+

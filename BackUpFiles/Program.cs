74f4349206f76ec2d2ff89a5d03793587e3988bb
AriesPL <blackhorns1989@gmail.com>
2022-07-27 18:48:02 +0300

    fead: Добавлен метод Логирования процесcов.

--- BackUpFiles/Program.cs
+++ BackUpFiles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace BackUpFiles
@@ -8,12 +9,16 @@ namespace BackUpFiles
 		static DateTime dataNow = DateTime.Now;
 		private static string pathToOriginalFolder = "C:/Users/Алексей/source/repos/BackUpFiles/BackUpFiles/original";
 		private static string pathToBackUpFolder = "BackUp/" + dataNow.ToShortDateString();
+		private static string pathToIogFile = "LogFile.txt";
 
 		static void Main(string[] args)
 		{
 			string fileName;
 			string sourceFile;
 			string destFile;
+
+			if (!File.Exists(pathToIogFile))
+				File.Create(pathToIogFile);
 
 			if (!Directory.Exists(pathToBackUpFolder))
 				Directory.CreateDirectory(pathToBackUpFolder);
@@ -28,14 +33,32 @@ namespace BackUpFiles
 					sourceFile = Path.Combine(pathToOriginalFolder, fileName);
 					destFile = Path.Combine(pathToBackUpFolder, fileName);
 					File.Copy(file, destFile, true);
+					Logbook($"{dataNow} Creating a copy of the file {fileName} successfully.\n");
 				}
 			}
+
 			else
 			{
 				Console.WriteLine("Source path does not exist!");
+				Logbook($"{dataNow} Source path does not exist!\n");
 			}
 
 			Console.ReadKey();
+		}
+
+		private static void Logbook(string records)
+		{
+			using (StreamWriter streamWriter = new StreamWriter(pathToIogFile, true))
+			{
+				try
+				{
+					streamWriter.WriteLine(records);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine("Exception: " + e.Message);
+				}
+			}
 		}
 	}
 }
